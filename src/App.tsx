363c53836b73c5290c110d44cab18b11c294ca80
Yazed Jamal <muyaszed@gmail.com>
2021-02-20 17:55:24 +0000

    Added main beat visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className="header"></div>
+        <div className="content-container">
+          <div className="beat-visual">
+            <div className="circle-shadow-animate"></div>
+
+            <div className="circle">
+              <div className="selected-bpm">72</div>
+            </div>
+          </div>
+          <div className="beat-selection"></div>
+          <div className="song-description"></div>
+        </div>
+      </div>
     </div>
   );
 }
